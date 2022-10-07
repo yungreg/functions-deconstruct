cedc59ca4339bfea3ad5517bbc7269364ca16fd9
Reggie Williams <rlwjr433@gmail.com>
2022-10-07 11:04:44 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+// task: practice an assigned guitar piece
+// step 1: Find the piece of music
+// Step 2: tune guitar
+// Step 3: find 8 measures of a page of music
+// Step 4: practice until you can play 3 times through with no mistakes. Move to step 5. 
+// Step 5: move on the next 8 bars and repeat step 4.
+
+const findSheetMusic = () => {
+    console.log('Grab some music from your Villa-Lobos book!')
+}
+
+const tuneYourGuitar = () => {
+    console.log('Great job finding youer music! Now, Tune your guitar!')
+}
+
+const findEightMeasures = () => {
+    console.log('Good job on tuning your guitar! Now, find the first 8 measures you want to practice!')
+}
+
+const practiceEightMeasures = () => {
+    console.log('Now that you know what you want to play, work on it until you can play it three times through, in a row, with no mistakes!')
+}
+
+const moveToNextEight = () => {
+    console.log('Great job on those last 8 measures! You\'re really getting this! Now, see if you can do the same thing for the next 8 measures! After you\'re done, move on to the next 8 bars and do the same!')
+}
+
+findSheetMusic()
+tuneYourGuitar()
+findEightMeasures()
+practiceEightMeasures()
+moveToNextEight()
